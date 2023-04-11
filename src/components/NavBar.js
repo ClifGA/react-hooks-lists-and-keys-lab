0bf1e88223ffabcf4372ae5ddbcf82285db02f0a
Clifton Gockul <clifga@gmail.com>
2023-04-10 21:46:24 -0400

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+ 
+  const aTagLinks = links.map(element => {
+    return <a key={element} href={'#'+ element}>{element}</a>
+  })
+  return <nav>{aTagLinks}</nav>;
 }
 
 export default NavBar;
